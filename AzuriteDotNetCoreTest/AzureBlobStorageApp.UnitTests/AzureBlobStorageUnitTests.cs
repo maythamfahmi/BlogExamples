3e741b5717087415acecafab342b0ddf264ec502
Maytham Fahmi <maythamfahmi@nextbix.dk>
2022-01-05 20:44:50 +0100

    Azure Storage UnitTest Part2

---
+++ AzuriteDotNetCoreTest/AzureBlobStorageApp.UnitTests/AzureBlobStorageUnitTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using Xunit;
+
+namespace AzureBlobStorageApp.UnitTests;
+
+public class AzureBlobStorageUnitTests
+{
+    private const string Content = "Some Data inside file Content";
+
+    private const string DefaultEndpointsProtocol = "http";
+    private const string AccountName = "devstoreaccount1";
+    private const string AccountKey = "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==";
+    private const string BlobEndpoint = "http://127.0.0.1:10000/devstoreaccount1";
+    private const string ConnectionString = $"DefaultEndpointsProtocol={DefaultEndpointsProtocol};AccountName={AccountName};AccountKey={AccountKey};BlobEndpoint={BlobEndpoint};";
+
+    private const string Container = "container-1";
+
+    private readonly AzureBlobStorage _azureBlobStorage;
+
+
+    public AzureBlobStorageUnitTests()
+    {
+        _azureBlobStorage = new AzureBlobStorage(ConnectionString, Container);
+    }
+
+    //https://docs.microsoft.com/en-us/azure/active-directory/managed-identities-azure-resources/overview
+
+    [Fact/*(Skip = "Works only on local environment")*/]
+    public async Task AzureBlobStorageTest()
+    {
+        // Arrange
+        await _azureBlobStorage?.CreateTextFile("file.txt", Encoding.UTF8.GetBytes(Content))!;
+
+        // Act
+        var readTextFile = await _azureBlobStorage.ReadTextFile("file.txt");
+
+        // Assert
+        Assert.Equal(Content, readTextFile);
+
+        // Finalizing
+        await _azureBlobStorage.DeleteTextFile("file.txt");
+    }
+
+}
---
+++ AzuriteDotNetCoreTest/AzureBlobStorageApp/AzureBlobStorage.cs
@@ -0,0 +1,38 @@
+﻿using Azure.Storage.Blobs;
+
+namespace AzureBlobStorageApp;
+
+public class AzureBlobStorage
+{
+
+    private readonly BlobContainerClient _blobContainerClient;
+
+    public AzureBlobStorage(string connectionString, string container)
+    {
+        _blobContainerClient = new BlobContainerClient(connectionString, container);
+        _blobContainerClient.CreateIfNotExists();
+    }
+
+    public async Task<string> ReadTextFile(string filename)
+    {
+        var blob = _blobContainerClient.GetBlobClient(filename);
+        if (!await _blobContainerClient.ExistsAsync()) return string.Empty;
+        var reading = await blob.DownloadStreamingAsync();
+        StreamReader reader = new StreamReader(reading.Value.Content);
+        return await reader.ReadToEndAsync();
+    }
+
+    public async Task CreateTextFile(string filename, byte[] data)
+    {
+        var blob = _blobContainerClient.GetBlobClient(filename);
+        await using var ms = new MemoryStream(data, false);
+        await blob.UploadAsync(ms, CancellationToken.None);
+    }
+
+    public async Task DeleteTextFile(string filename)
+    {
+        var blobClient = _blobContainerClient.GetBlobClient(filename);
+        await blobClient.DeleteAsync();
+    }
+
+}
