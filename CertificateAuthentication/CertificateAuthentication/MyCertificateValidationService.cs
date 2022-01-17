3cfd3f419583eec175bb73a84fae5324a239626a
Maytham Fahmi <maythamfahmi@nextbix.dk>
2022-01-17 11:28:33 +0100

    init

---
+++ CertificateAuthentication/CertificateAuthentication/MyCertificateValidationService.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+public class MyCertificateValidationService
+{
+    public bool ValidateCertificate(X509Certificate2 clientCertificate)
+    {
+        var cert = new X509Certificate2(@"D:\certs\localhost.pfx");
+        if (clientCertificate.Thumbprint == cert.Thumbprint)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
